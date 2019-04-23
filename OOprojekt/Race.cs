a974960c78a3472bb042b5e11889450bc7e3cc9a
Benjamin Larsen <benjamin.ettrup.larsen@gmail.com>
2019-04-23 16:35:37 +0200

    Added all the files and made version 1

---
+++ OOprojekt/Race.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOprojekt
+{
+    class Race
+    {
+
+        //====================
+        //  CLASS VARIABLES
+        //====================
+        private string[] racer;
+
+        //=========================
+        //  DEFAULT CONSTRUCTOR
+        //=========================
+        public Race()
+        {
+            racer = new string[3];
+            racer[0] = "Ork";
+            racer[1] = "Elf";
+            racer[2] = "Human";
+
+
+        }
+
+        //==============================
+        //  READ AND WRITE PROPERTY
+        //==============================
+
+        public int() {
+
+            }
+
+    }
+}
