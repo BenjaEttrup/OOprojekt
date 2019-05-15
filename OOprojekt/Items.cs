5aa4728e9c249cd2c6e3fccb4251b7c9f5e2a4b9
Benjamin Larsen <benjamin.ettrup.larsen@gmail.com>
2019-05-15 21:23:17 +0200

    Lavet alt det basale af itemsystemet og tilføjet kommentare.

---
+++ OOprojekt/Items.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOprojekt
+{
+    class Items
+    {
+
+        //====================
+        //  CLASS VARIABLES
+        //====================
+
+        private string itemName;
+
+        //Værdien i denne variabel styrer hvor meget der skal lægges til eller trækkes fra spillerens liv
+        private int healthEffect;
+
+        //Værdien i denne variabel styrer hvor meget der skal lægges til eller trækkes fra spillerens CP
+        private int CPEffect;
+
+
+        //=========================
+        //  DEFAULT CONSTRUCTOR
+        //=========================
+
+        //Constructoren er den kode der bliver kørt når et objekt bliver lavet af classen
+        public Items()
+        {
+            //Sætter variablen til 0 så hvis ikke den bliver ændret så sker der ikke noget
+            healthEffect = 0;
+            CPEffect = 0;
+        }
+        
+        
+        //=========================
+        //      METHODS
+        //=========================
+
+        //Laver en metode som tager imod en string parameter
+        private void ItemChoosen(string itemName)
+        {
+            //Laver en switch som skal mache navnet på Itemet så programmet ved hvad den skal gemme i hvilke variabler
+            switch (itemName)
+            {
+                //Dette er de forskellige Items
+                case "Apple":
+                    healthEffect = 20;
+                    break;
+
+                case "Sword":
+                    CPEffect = 20;
+                    break;
+                    
+                
+            }
+        }
+
+
+        //==============================
+        //  READ AND WRITE PROPERTY
+        //==============================
+
+        //Propertyen giver ItemChoosen metoden en værdi
+        public string ItemNameProp
+        {
+            set { ItemChoosen(value); itemName = value; }
+            get { return itemName; }
+        }
+
+        //Propertyen gør så GameFormen kan få information om hvor meget Health og CP skal ændres
+        public int HealthEffectProp
+        {
+            get { return healthEffect; }
+        }
+        public int CPEffectProp
+        {
+            get { return CPEffect; }
+        }
+    }
+}
